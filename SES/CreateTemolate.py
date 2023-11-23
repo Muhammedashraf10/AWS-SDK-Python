9b0e65c0211c2fc2db86257a3db7d1065acad254
Muhammed Ashraf <muhammedashraf192@gmail.com>
2023-11-23 13:06:28 +0200

    Update CreateTemolate.py

--- SES/CreateTemolate.py
+++ SES/CreateTemolate.py
@@ -4,10 +4,10 @@ ses_client = boto3.client('ses')
 
 response = ses_client.create_template(
     Template={
-        'TemplateName': 'CustomTemplate',
-        'SubjectPart': 'Welcome to the Course',
-        'TextPart': 'Thanks for buying',
-        'HtmlPart': 'Thanks for buying'
+        'TemplateName': 'TemplateName',
+        'SubjectPart': 'Welcome ',
+        'TextPart': 'Text Part of the Template',
+        'HtmlPart': 'Html Part of the Template'
     }
     
 )
