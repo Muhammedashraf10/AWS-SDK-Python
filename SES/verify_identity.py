d07edce22b87e6300f30dfeeda20ffa25f68ee2b
Muhammed Ashraf <muhammedashraf192@gmail.com>
2023-11-23 13:12:02 +0200

    Update verify_identity.py

--- SES/verify_identity.py
+++ SES/verify_identity.py
@@ -2,7 +2,7 @@ import boto3
 
 ses_client = boto3.client('ses')
 response = ses_client.verify_email_address(
-    EmailAddress='muhammedashraf192@gmail.com'
+    EmailAddress='xxxxx@yyyy.com' #Replace it with your email address in order to verify it on AWS
     )
     
 print(response)
