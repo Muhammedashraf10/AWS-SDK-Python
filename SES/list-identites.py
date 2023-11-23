374621b18b10ce06b28129d30133bd4e3d786a17
Muhammed Ashraf <muhammedashraf192@gmail.com>
2023-11-23 13:08:52 +0200

    Update list-identites.py

--- SES/list-identites.py
+++ SES/list-identites.py
@@ -3,7 +3,7 @@ import boto3
 ses_client = boto3.client('ses')
 
 response = ses_client.list_identities(
-    IdentityType = 'EmailAddress'
+    IdentityType = 'EmailAddress' #List all verified Identites
     
     )
     
