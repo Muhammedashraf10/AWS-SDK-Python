c06a07450259d638737b618a726194ed3834169c
Muhammed Ashraf <muhammedashraf192@gmail.com>
2023-11-23 13:11:29 +0200

    Update send_email.py

--- SES/send_email.py
+++ SES/send_email.py
@@ -2,13 +2,13 @@ import boto3
 
 ses_client = boto3.client('ses')
 response = ses_client.send_templated_email(
-    Source= 'muhammedashraf192@gmail.com',
+    Source= 'wwww@yyyy.com', #Replace it with your verified email address at AWS SES
     Destination={
-        'ToAddresses':['eng.muhammadashraf1@gmail.com'],
-        'CcAddresses':['muhammedashraf192@gmail.com']
+        'ToAddresses':['xxxxx@zzzz.com'], #Enter your destination, you can use list of emails as destintation ['xxxxx@zzzz.com', 'xxxxx2@zzzz.com'],
+        'CcAddresses':['wwww@yyyy.com']
     },
-    ReplyToAddresses=['muhammedashraf192@gmail.com'],
-    Template='CustomTemplate',
+    ReplyToAddresses=['wwww@yyyy.com'],
+    Template='CustomTemplate', #Replace it with your predefined template
     TemplateData='{"replace":"value"}'
     
 )
