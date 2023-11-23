89a80e8183d048da69a1ebd9daca54483127b78e
Muhammed Ashraf <muhammedashraf192@gmail.com>
2023-11-23 13:08:06 +0200

    Update GetTemplate.py

--- SES/GetTemplate.py
+++ SES/GetTemplate.py
@@ -4,7 +4,7 @@ from pprint import pprint
 ses_client = boto3.client('ses')
 
 response = ses_client.get_template(
-    TemplateName='CustomTemplate'
+    TemplateName='CustomTemplate' #Replace it with Template name to get the template information
     )
     
 pprint(response)
