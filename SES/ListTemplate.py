dedce3bbb0b28f4ee6d2bb75bfb93f0135ef0450
Muhammed Ashraf <muhammedashraf192@gmail.com>
2023-11-23 13:08:36 +0200

    Update ListTemplate.py

--- SES/ListTemplate.py
+++ SES/ListTemplate.py
@@ -3,10 +3,10 @@ from pprint import pprint
 
 ses_client = boto3.client('ses')
 
-response = ses_client.get_template(
-    TemplateName='CustomTemplate'
+response = ses_client.get_template(  #Get a specific template 
+    TemplateName='CustomTemplate' 
     )
-pprint(response['Template'])
+pprint(response['Template']) 
 
-response_2 = ses_client.list_templates()
+response_2 = ses_client.list_templates() # List all existing templates
 pprint(response_2['TemplatesMetadata'])
