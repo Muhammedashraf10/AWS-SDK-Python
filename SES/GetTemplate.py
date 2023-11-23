14278ce2c8f28a6b0cc27ffa5d2e8e029cabc450
Muhammed Ashraf <muhammedashraf192@gmail.com>
2023-11-23 11:54:10 +0200

    Create GetTemplate.py

---
+++ SES/GetTemplate.py
@@ -0,0 +1,10 @@
+import boto3
+from pprint import pprint 
+
+ses_client = boto3.client('ses')
+
+response = ses_client.get_template(
+    TemplateName='CustomTemplate'
+    )
+    
+pprint(response)
