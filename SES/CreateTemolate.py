42d85d3036c831e440f059efff8dcd43bc280068
Muhammed Ashraf <muhammedashraf192@gmail.com>
2023-11-23 13:07:41 +0200

    Update CreateTemolate.py

--- SES/CreateTemolate.py
+++ SES/CreateTemolate.py
@@ -4,10 +4,10 @@ ses_client = boto3.client('ses')
 
 response = ses_client.create_template(
     Template={
-        'TemplateName': 'TemplateName',
-        'SubjectPart': 'Welcome ',
-        'TextPart': 'Text Part of the Template',
-        'HtmlPart': 'Html Part of the Template'
+        'TemplateName': 'TemplateName', #Enter the Template Name
+        'SubjectPart': 'Welcome ', # Enter the Subject that will be used
+        'TextPart': 'Text Part of the Template', #Replace the Template Text
+        'HtmlPart': 'Html Part of the Template' #Replace the Html Text
     }
     
 )
